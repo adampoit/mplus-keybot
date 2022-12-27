39284ae13b103461f93b8c34543b02c5848de99d
Adam Poit <1825301+adampoit@users.noreply.github.com>
2022-12-26 21:18:51 -0800

    Update deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 ssh root@example.com 'mkdir -p /srv/mplus-keybot'
+
+ssh root@example.com 'sudo systemctl stop mplus-keybot.service'
+
 scp artifacts/* root@example.com:/srv/mplus-keybot
 scp mplus-keybot.service root@example.com:/etc/systemd/system
+
+ssh root@example.com 'sudo systemctl daemon-reload'
+ssh root@example.com 'sudo systemctl start mplus-keybot.service'
+ssh root@example.com 'sudo systemctl status mplus-keybot.service'
